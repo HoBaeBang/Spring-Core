e5a236ab05983f10bddd77d8af80e6475dc34249
B-aslan0 <bhb0407@naver.com>
2021-08-11 00:18:58 +0900

    빈 조회하기

--- core/src/test/java/hello/core/beanfind/ApplicationContextBasicFindTest.java
+++ core/src/test/java/hello/core/beanfind/ApplicationContextBasicFindTest.java
@@ -33,7 +33,7 @@ public class ApplicationContextBasicFindTest {
     @Test
     @DisplayName("구체 타입으로만 조회")
     void findBeanByType2(){
-        MemberService memberService = ac.getBean("memberService", MemberServiceImpl.class);
+        MemberServiceImpl memberService = ac.getBean("memberService", MemberServiceImpl.class);
         assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
     }
 
